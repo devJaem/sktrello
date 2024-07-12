9e4187b20878233b23273b5c18c13f19c4a0cb74
jaemin <wjdwoals9292@gmail.com>
2024-07-12 12:38:27 +0900

    🎉 feat: checklist, checkitem entity add

--- src/check_item/entities/check_item.entity.ts
+++ src/check_item/entities/check_item.entity.ts
@@ -1,1 +1,45 @@
-export class CheckItem {}
+import { CheckList } from 'src/check_list/entities/check_list.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('check_item')
+export class CheckItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  checkListId: number;
+
+  @Column()
+  content: string;
+
+  @Column()
+  isDone: boolean;
+
+  @Column()
+  checkItemOrder: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  @ManyToOne(() => CheckList, (checklist) => checklist.checkItems, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'check_list_id', referencedColumnName: 'id' }])
+  checklist: CheckList;
+}
