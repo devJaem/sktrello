9030bbe75ae4515e9794d92e6ea376b4553e62d1
xszvvfm <bce0102@gmail.com>
2024-07-12 13:20:46 +0900

    � feat:list-entity

--- src/list/entities/list.entity.ts
+++ src/list/entities/list.entity.ts
@@ -1,1 +1,41 @@
-export class List {}
+import { Board } from 'src/board/entities/board.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('lists')
+export class List {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column()
+  boardId: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  listOrder: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  // boards 테이블과 N:1 관계
+  // @ManyToOne(() => Board, (board) => board.lists, {
+  //   onDelete: 'CASCADE',
+  //   onUpdate: 'CASCADE',
+  // })
+  // board: Board;
+}
