3fe1666550dc961c206b0b94db35fbaa4eac41b3
CDR4733 <shsing2@naver.com>
2024-07-13 23:00:20 +0900

    🎉feat:Board-CRUD,초대기능
    List 당겨오기 기능을 제외한 모든 기능 구현 완료

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -6,37 +6,110 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
 import { BoardService } from './board.service';
+
+import { MESSAGES } from 'src/constants/message.constants';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { InviteBoardMemberDto } from './dto/invite-board-member.dto';
 
-@Controller('board')
+@Controller('boards')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
-  @Post()
-  create(@Body() createBoardDto: CreateBoardDto) {
-    return this.boardService.create(createBoardDto);
+  /** Board 생성(C) API **/
+  @Post('')
+  async createBoard(@Body() createBoardDto: CreateBoardDto) {
+    const createdBoard = await this.boardService.createBoard(
+      user,
+      createBoardDto
+    );
+    const result = {
+      status: HttpStatus.CREATED,
+      message: MESSAGES.BOARD.CREATE.SUCCESS,
+      data: createdBoard,
+    };
+    return result;
   }
 
-  @Get()
-  findAll() {
-    return this.boardService.findAll();
+  /** Board 목록 조회(R-L) **/
+  @Get('')
+  async findAllBoard() {
+    const foundAllBoard = await this.boardService.findAllBoard(user);
+    const result = {
+      status: HttpStatus.OK,
+      message: MESSAGES.BOARD.READ_LIST.SUCCESS,
+      data: {
+        participatedIn: foundAllBoard,
+      },
+    };
+    return result;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardService.findOne(+id);
+  /** Board 상세 조회(R-D) **/
+  @Get(':boardId')
+  async findOneBoard(@Param('boardId') boardId: number) {
+    const foundOneBoard = await this.boardService.findOneBoard(user, boardId);
+    const result = {
+      status: HttpStatus.OK,
+      message: MESSAGES.BOARD.READ_DETAIL.SUCCESS,
+      data: foundOneBoard,
+    };
+    return result;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-    return this.boardService.update(+id, updateBoardDto);
+  /** Board 수정(U) API **/
+  @Patch(':boardId')
+  async updateBoard(
+    @Param('boardId') boardId: number,
+    @Body() updateBoardDto: UpdateBoardDto
+  ) {
+    const updatedBoard = await this.boardService.updateBoard(
+      user,
+      boardId,
+      updateBoardDto
+    );
+    const result = {
+      status: HttpStatus.OK,
+      message: MESSAGES.BOARD.UPDATE.SUCCESS,
+      data: updatedBoard,
+    };
+    return result;
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.boardService.remove(+id);
+  /** Board 삭제(D) API **/
+  @Delete(':boardId')
+  async softDeleteBoard(@Param('boardId') boardId: number) {
+    const deletedBoard = await this.boardService.softDeleteBoard(user, boardId);
+    const result = {
+      status: HttpStatus.OK,
+      message: MESSAGES.BOARD.DELETE.SUCCESS,
+      data: deletedBoard,
+    };
+    return result;
+  }
+
+  /** Board 멤버 초대(Invite) API **/
+  @Post(':boardId/invite')
+  async inviteBoardMember(
+    @Param('boardId') boardId: number,
+    @Body() inviteBoardMemberDto: InviteBoardMemberDto
+  ) {
+    const invitedMember = await this.boardService.inviteBoardMember(
+      user,
+      boardId,
+      inviteBoardMemberDto
+    );
+    const result = {
+      status: HttpStatus.CREATED,
+      message: MESSAGES.BOARD.INVITE.SUCCESS,
+      data: {
+        nickname: invitedMember.nickname,
+        email: invitedMember.email,
+      },
+    };
+    return result;
   }
 }
--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -4,9 +4,10 @@ import { BoardController } from './board.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
 import { BoardUser } from './entities/board-user.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board, BoardUser])],
+  imports: [TypeOrmModule.forFeature([Board, BoardUser, User])],
   controllers: [BoardController],
   providers: [BoardService],
   exports: [BoardService],
