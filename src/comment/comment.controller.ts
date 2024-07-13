89d27411e47ada9a7527acc960761b7b7db0e2b2
hyein <mickey_8688@naver.com>
2024-07-14 03:24:25 +0900

    🎉 feat:댓글 CRUD 동작확인완료

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -6,37 +6,54 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
-@Controller('comment')
+@Controller('cards/:cardId/comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentService.create(createCommentDto);
+  async create(
+    @Param('cardId', ParseIntPipe) cardId: number,
+    @Body() createCommentDto: CreateCommentDto
+  ) {
+    const userId = 1; // 사용자 ID 하드코딩
+    return this.commentService.create(createCommentDto, userId, cardId);
   }
 
   @Get()
-  findAll() {
-    return this.commentService.findAll();
+  findAll(@Param('cardId', ParseIntPipe) cardId: number) {
+    return this.commentService.findAll(cardId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
+  findOne(
+    @Param('cardId', ParseIntPipe) cardId: number,
+    @Param('id', ParseIntPipe) id: number
+  ) {
+    return this.commentService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
+  update(
+    @Param('cardId', ParseIntPipe) cardId: number,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCommentDto: UpdateCommentDto
+  ) {
+    const userId = 1; // 사용자 ID 하드 코딩
+    return this.commentService.update(id, updateCommentDto, userId);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+  remove(
+    @Param('cardId', ParseIntPipe) cardId: number,
+    @Param('id') id: number
+  ) {
+    const userId = 1; // 사용자 ID 하드 코딩
+    return this.commentService.remove(id, userId);
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Repository } from 'typeorm';
+import { Comment } from 'src/comment/entities/comment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CommentService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentReporsitory: Repository<Comment>
+  ) {}
+  async create(
+    createCommentDto: CreateCommentDto,
+    userId: number,
+    cardId: number
+  ) {
+    const { content } = createCommentDto;
+    const comment = this.commentReporsitory.create({ userId, cardId, content });
+    return comment;
   }
 
-  findAll() {
-    return `This action returns all comment`;
+  async findAll(cardId: number) {
+    return await this.commentReporsitory.find({ where: { cardId } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  async findOne(id: number) {
+    // 주어진 id에 해당하는 카드 조회
+    return await this.commentReporsitory.findOne({
+      where: { id },
+    });
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(id: number, updateCommentDto: UpdateCommentDto, userId: number) {
+    await this.commentReporsitory.update({ id, userId }, updateCommentDto);
+    return this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(id: number, userId: number) {
+    await this.commentReporsitory.delete({ id, userId });
+    return { deleted: true };
   }
 }
