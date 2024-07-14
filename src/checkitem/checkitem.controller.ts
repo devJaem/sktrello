46a90bf2d40cbb2742bdec04ed150b3d54b05aa1
jaemin <wjdwoals9292@gmail.com>
2024-07-14 20:31:25 +0900

    🎉 feat: CheckItem 초안 작성

--- src/checkitem/checkitem.controller.ts
+++ src/checkitem/checkitem.controller.ts
@@ -6,40 +6,109 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
 import { CheckItemService } from './checkItem.service';
 import { CreateCheckItemDto } from './dto/create-checkItem.dto';
 import { UpdateCheckItemDto } from './dto/update-checkItem.dto';
+import { MoveCheckItemDto } from './dto/move-checkItem.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { MESSAGES } from '../constants/check-message.constats'; // 메시지 상수 파일 가져오기
 
-@Controller('checkItems')
+@Controller('checklists/:checklistId/items')
 export class CheckItemController {
   constructor(private readonly checkItemService: CheckItemService) {}
 
   @Post()
-  create(@Body() createCheckItemDto: CreateCheckItemDto) {
-    return this.checkItemService.create(createCheckItemDto);
+  async create(
+    @Param('checklistId') checklistId: number,
+    @Body() createCheckItemDto: CreateCheckItemDto
+  ) {
+    const newCheckItem = await this.checkItemService.create({
+      ...createCheckItemDto,
+      checkListId: checklistId,
+    });
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: MESSAGES.CHECKITEM.CREATE,
+      data: newCheckItem,
+    };
   }
 
   @Get()
-  findAll() {
-    return this.checkItemService.findAll();
+  async findAll(@Param('checklistId') checklistId: number) {
+    const checkItems = await this.checkItemService.findAll(checklistId);
+    return {
+      statusCode: HttpStatus.OK,
+      data: checkItems,
+    };
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.checkItemService.findOne(+id);
+  @Get(':itemId')
+  async findOne(@Param('itemId') itemId: number) {
+    const checkItem = await this.checkItemService.findOne(itemId);
+    return {
+      statusCode: HttpStatus.OK,
+      data: checkItem,
+    };
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
+  @Patch(':itemId')
+  async update(
+    @Param('itemId') itemId: number,
     @Body() updateCheckItemDto: UpdateCheckItemDto
   ) {
-    return this.checkItemService.update(+id, updateCheckItemDto);
+    const updatedCheckItem = await this.checkItemService.update(
+      itemId,
+      updateCheckItemDto
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: MESSAGES.CHECKITEM.UPDATE,
+      data: updatedCheckItem,
+    };
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.checkItemService.remove(+id);
+  @Delete(':itemId')
+  async remove(@Param('itemId') itemId: number) {
+    const deletedCheckItem = await this.checkItemService.remove(itemId);
+    return {
+      statusCode: HttpStatus.OK,
+      message: MESSAGES.CHECKITEM.DELETE,
+      data: deletedCheckItem,
+    };
+  }
+
+  @Patch(':itemId/move')
+  async moveItemWithinCheckList(
+    @Param('itemId') itemId: number,
+    @Body() moveCheckItemDto: MoveCheckItemDto
+  ) {
+    const movedCheckItem = await this.checkItemService.moveItemWithinCheckList(
+      itemId,
+      moveCheckItemDto
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: MESSAGES.CHECKITEM.MOVE_WITHIN,
+      data: movedCheckItem,
+    };
+  }
+
+  @Patch(':itemId/move-to-another')
+  async moveItemToAnotherChecklist(
+    @Param('itemId') itemId: number,
+    @Body() moveCheckItemDto: MoveCheckItemDto
+  ) {
+    const movedCheckItem =
+      await this.checkItemService.moveItemToAnotherChecklist(
+        itemId,
+        moveCheckItemDto
+      );
+    return {
+      statusCode: HttpStatus.OK,
+      message: MESSAGES.CHECKITEM.MOVE_TO_ANOTHER,
+      data: movedCheckItem,
+    };
   }
 }
--- src/checkitem/checkitem.service.ts
+++ src/checkitem/checkitem.service.ts
@@ -1,33 +1,38 @@
 import _ from 'lodash';
 import { CreateCheckItemDto } from './dto/create-checkItem.dto';
 import { UpdateCheckItemDto } from './dto/update-checkItem.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CheckItem } from './entities/checkItem.entity';
-import { DataSource, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { LexoRank } from 'lexorank';
+import { MoveCheckItemDto } from './dto/move-checkItem.dto';
+import { MESSAGES } from '../constants/check-message.constats'
 
 @Injectable()
 export class CheckItemService {
   constructor(
     @InjectRepository(CheckItem)
-    private readonly checkItemRepository: Repository<CheckItem>,
-    private readonly dataSource: DataSource
+    private readonly checkItemRepository: Repository<CheckItem>
   ) {}
 
+  /**
+   * 체크리스트 내부 아이탬 추가
+   * @param createCheckItemDto 체크아이탬 생성 DTO
+   * @returns 생성된 결과값
+   */
   async create(createCheckItemDto: CreateCheckItemDto) {
     const { checkListId, content } = createCheckItemDto;
 
-    const lasCheckItem = await this.checkItemRepository.findOne({
+    const lastCheckItem = await this.checkItemRepository.findOne({
       where: { checkListId },
       order: { checkItemOrder: 'DESC' },
     });
 
-    let order = LexoRank.middle();
-    if (lasCheckItem) {
-      const lastOrder = LexoRank.parse(lasCheckItem.checkItemOrder);
-      order = lastOrder.genNext();
-    }
+    // RexoRank
+    const order = lastCheckItem
+      ? LexoRank.parse(lastCheckItem.checkItemOrder).genNext()
+      : LexoRank.middle();
 
     const newCheckItem = this.checkItemRepository.create({
       checkListId,
@@ -38,19 +43,172 @@ export class CheckItemService {
     return newCheckItem;
   }
 
-  findAll() {
-    return `This action returns all checkItem`;
+  /**
+   * 체크리스트 내부 아이탬 전체 조회
+   * @param checkListId 체크리스트 ID
+   * @returns 조회된 결과값
+   */
+  async findAll(checkListId: number) {
+    const checkItems = await this.checkItemRepository
+      .createQueryBuilder('check_item')
+      .where('check_item.checklist_id = :checklistId', { checkListId })
+      .getRawMany();
+
+    return checkItems;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} checkItem`;
+  /**
+   * 체크아이탬 단일 조회
+   * @param id 체크아이템 ID
+   * @returns 조회된 결과값
+   */
+  async findOne(id: number) {
+    const checkItem = await this.verifyListById(id);
+    return checkItem;
   }
 
-  update(id: number, updateCheckItemDto: UpdateCheckItemDto) {
-    return `This action updates a #${id} checkItem`;
+  /**
+   * 체크아이탬 수정
+   * @param id 체크아이템 ID
+   * @param updateCheckItemDto  content or isDone
+   */
+  async update(id: number, updateCheckItemDto: UpdateCheckItemDto) {
+    await this.verifyListById(id);
+    await this.checkItemRepository.update({ id }, updateCheckItemDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} checkItem`;
+  /**
+   * 체크리스트 내의 아이탬 순서이동
+   * @param id 체크아이템 ID
+   * @param moveCheckItemDto targetOrder : 이동할 곳의 목표 순서값
+   * @returns 이동한 결과값
+   */
+  async moveItemWithinCheckList(
+    id: number,
+    moveCheckItemDto: MoveCheckItemDto
+  ) {
+    const { targetOrder } = moveCheckItemDto;
+
+    const checkItem = await this.verifyListById(id);
+    const targetItem = await this.checkItemRepository.findOne({
+      where: {
+        checkListId: checkItem.checkListId,
+        checkItemOrder: targetOrder.toString(),
+      },
+    });
+
+    if (_.isNil(targetItem)) {
+      throw new NotFoundException(MESSAGES.CHECKITEM.TARGET_NOT_FOUND);
+    }
+
+    const targetRank = LexoRank.parse(targetItem.checkItemOrder);
+    const currentRank = LexoRank.parse(checkItem.checkItemOrder);
+    const newRank = targetRank.between(currentRank);
+
+    checkItem.checkItemOrder = newRank.toString();
+    await this.checkItemRepository.save(checkItem);
+
+    return { originalItem: checkItem, targetItem: targetItem };
+  }
+
+  /**
+   * 다른 체크리스트로 이동 and 위치까지 지정
+   * @param id 체크아이템 ID
+   * @param moveCheckItemDto targetChecklistId, targetOrder
+   * @returns 이동한 결과값
+   */
+  async moveItemToAnotherChecklist(
+    id: number,
+    moveCheckItemDto: MoveCheckItemDto
+  ) {
+    const { targetChecklistId, targetOrder } = moveCheckItemDto;
+
+    // 이동할 체크아이템 가져오기
+    const checkItem = await this.verifyListById(id);
+    checkItem.checkListId = targetChecklistId;
+
+    let newOrder = checkItem.checkItemOrder; // 기본적으로 현재 순서를 유지
+
+    if (targetOrder) {
+      const targetItem = await this.checkItemRepository.findOne({
+        where: {
+          checkListId: targetChecklistId,
+          checkItemOrder: targetOrder.toString(),
+        },
+      });
+
+      if (_.isNil(targetItem)) {
+        throw new NotFoundException(MESSAGES.CHECKITEM.NOT_FOUND);
+      }
+
+      const targetRank = LexoRank.parse(targetItem.checkItemOrder);
+      const currentRank = LexoRank.parse(checkItem.checkItemOrder);
+
+      // 기존의 순서와 목표 순서가 동일하지 않을 경우에만 새로운 순서를 생성
+      if (!currentRank.equals(targetRank)) {
+        newOrder = targetRank.genNext().toString();
+      }
+    } else {
+      // 타겟 체크리스트에서 마지막 아이템 순서 가져오기
+      const lastCheckItemInTargetList = await this.checkItemRepository.findOne({
+        where: { checkListId: targetChecklistId },
+        order: { checkItemOrder: 'DESC' },
+      });
+
+      // LexoRank를 사용하여 새로운 순서 생성
+      newOrder = lastCheckItemInTargetList
+        ? LexoRank.parse(lastCheckItemInTargetList.checkItemOrder)
+            .genNext()
+            .toString()
+        : LexoRank.middle().toString();
+    }
+
+    // 새로운 순서와 체크리스트 ID 저장
+    checkItem.checkItemOrder = newOrder;
+    await this.checkItemRepository.save(checkItem);
+
+    return { originalItem: checkItem, targetList: targetChecklistId };
+  }
+
+  /**
+   * 체크 아이탬 삭제
+   * @param id 체크아이템 ID
+   * @returns 소프트 딜리트 된 아이탬 리턴
+   */
+  async remove(id: number) {
+    const checkItem = await this.verifyListById(id);
+    checkItem.deletedAt = new Date();
+    await this.checkItemRepository.save(checkItem);
+    return { deletedItem: checkItem };
+  }
+
+  /**
+   * 한개의 id값을 열람하는 메서드
+   * @param id 체크아이템 ID
+   * @returns id에 해당하는 컬럼
+   */
+  private async verifyListById(id: number) {
+    const checkItem = await this.checkItemRepository.findOne({
+      where: { id },
+    });
+    if (_.isNil(checkItem)) {
+      throw new NotFoundException(MESSAGES.CHECKITEM.NOT_FOUND);
+    }
+
+    return checkItem;
+  }
+  /**
+   * 전체 list 갯수 확인 메서드
+   * @param checkListId 체크리스트 id
+   * @returns list 갯수
+   */
+  async count(checkListId: number) {
+    const listCount = await this.checkItemRepository
+      .createQueryBuilder('check_item')
+      .select('COUNT(check_item.checkItemOrder)', 'total_list_count')
+      .where('check_item.checkListId = :checkListId', { checkListId })
+      .getRawOne();
+
+    return listCount;
   }
 }
