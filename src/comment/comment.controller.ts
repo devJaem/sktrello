73db64052e6daaa703518bf351a1edf8b94e3bd0
hyein <mickey_8688@naver.com>
2024-07-15 10:14:22 +0900

    🩹 fix: 댓글 컨트롤러, 서비스 수정, 메시지 생성

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -7,53 +7,94 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  HttpStatus,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { COMMENT_MESSAGE } from 'src/constants/comment.message.constants';
 
 @Controller('cards/:cardId/comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  // 댓글 생성
   @Post()
   async create(
     @Param('cardId', ParseIntPipe) cardId: number,
     @Body() createCommentDto: CreateCommentDto
   ) {
     const userId = 1; // 사용자 ID 하드코딩
-    return this.commentService.create(createCommentDto, userId, cardId);
+    const createdComment = await this.commentService.create(
+      createCommentDto,
+      userId,
+      cardId
+    );
+    return {
+      status: HttpStatus.CREATED,
+      message: COMMENT_MESSAGE.COMMENT.CREATE.SUCCESS,
+      data: createdComment,
+    };
   }
 
+  // 댓글 조회
   @Get()
-  findAll(@Param('cardId', ParseIntPipe) cardId: number) {
-    return this.commentService.findAll(cardId);
+  async findAll(@Param('cardId', ParseIntPipe) cardId: number) {
+    const readAllComment = await this.commentService.findAll(cardId);
+    return {
+      status: HttpStatus.OK,
+      message: COMMENT_MESSAGE.COMMENT.READ_ALL.SUCCESS,
+      data: readAllComment,
+    };
   }
 
+  // 댓글 상세 조회
   @Get(':id')
-  findOne(
+  async findOne(
     @Param('cardId', ParseIntPipe) cardId: number,
     @Param('id', ParseIntPipe) id: number
   ) {
-    return this.commentService.findOne(id);
+    const userId = 1; // 사용자 ID 하드 코딩
+    const readDetailComment = await this.commentService.findOne(id, userId);
+    return {
+      status: HttpStatus.OK,
+      message: COMMENT_MESSAGE.COMMENT.READ_DETAIL.SUCCESS,
+      data: readDetailComment,
+    };
   }
 
+  // 댓글 수정
   @Patch(':id')
-  update(
+  async update(
     @Param('cardId', ParseIntPipe) cardId: number,
     @Param('id', ParseIntPipe) id: number,
     @Body() updateCommentDto: UpdateCommentDto
   ) {
     const userId = 1; // 사용자 ID 하드 코딩
-    return this.commentService.update(id, updateCommentDto, userId);
+    const updateComment = await this.commentService.update(
+      id,
+      updateCommentDto,
+      userId
+    );
+    return {
+      status: HttpStatus.OK,
+      message: COMMENT_MESSAGE.COMMENT.UPDATE.SUCCESS,
+      data: updateComment,
+    };
   }
 
+  // 댓글 삭제
   @Delete(':id')
-  remove(
+  async remove(
     @Param('cardId', ParseIntPipe) cardId: number,
     @Param('id') id: number
   ) {
     const userId = 1; // 사용자 ID 하드 코딩
-    return this.commentService.remove(id, userId);
+    const deleteComment = await this.commentService.remove(id, userId);
+    return {
+      status: HttpStatus.OK,
+      message: COMMENT_MESSAGE.COMMENT.DELETE.SUCCESS,
+      data: deleteComment,
+    };
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,44 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { Repository } from 'typeorm';
 import { Comment } from 'src/comment/entities/comment.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { COMMENT_MESSAGE } from 'src/constants/comment.message.constants';
 
 @Injectable()
 export class CommentService {
   constructor(
     @InjectRepository(Comment)
-    private readonly commentReporsitory: Repository<Comment>
+    private readonly commentRepository: Repository<Comment>
   ) {}
+  // 댓글 생성
   async create(
     createCommentDto: CreateCommentDto,
     userId: number,
     cardId: number
   ) {
     const { content } = createCommentDto;
-    const comment = this.commentReporsitory.create({ userId, cardId, content });
+    const comment = this.commentRepository.save({ userId, cardId, content });
     return comment;
   }
 
+  // 댓글 조회
   async findAll(cardId: number) {
-    return await this.commentReporsitory.find({ where: { cardId } });
+    return await this.commentRepository.find({ where: { cardId } });
   }
 
-  async findOne(id: number) {
-    // 주어진 id에 해당하는 카드 조회
-    return await this.commentReporsitory.findOne({
-      where: { id },
+  // 댓글 상세 조회
+  async findOne(id: number, userId: number) {
+    // 주어진 id에 해당하는 댓글 조회
+    const comment = await this.commentRepository.findOne({
+      where: { id, deletedAt: null },
     });
+    // 댓글 작성자가 본인인지 확인
+    if (userId !== comment.userId) {
+      throw new ForbiddenException('권한이 없습니다.');
+    }
+
+    return comment;
   }
 
+  // 댓글 수정
   async update(id: number, updateCommentDto: UpdateCommentDto, userId: number) {
-    await this.commentReporsitory.update({ id, userId }, updateCommentDto);
-    return this.findOne(id);
+    const comment = await this.commentRepository.findOne({
+      where: { id, deletedAt: null },
+    });
+
+    // 댓글이 존재하는지 확인
+    if (!comment) {
+      throw new NotFoundException(
+        COMMENT_MESSAGE.COMMENT.READ_DETAIL.NOT_FOUND
+      );
+    }
+
+    // 댓글 작성자가 본인인지 확인
+    if (userId !== comment.userId) {
+      throw new ForbiddenException('권한이 없습니다.');
+    }
+
+    await this.commentRepository.update({ id, userId }, updateCommentDto);
+    return this.findOne(id, userId);
   }
 
+  // 댓글 삭제
   async remove(id: number, userId: number) {
-    await this.commentReporsitory.delete({ id, userId });
+    const comment = await this.commentRepository.findOne({
+      where: { id, deletedAt: null },
+    });
+    // 댓글이 존재하는지 확인
+    if (!comment) {
+      throw new NotFoundException(
+        COMMENT_MESSAGE.COMMENT.READ_DETAIL.NOT_FOUND
+      );
+    }
+    // 댓글 작성자가 본인인지 확인
+    if (userId !== comment.userId) {
+      throw new ForbiddenException('권한이 없습니다.');
+    }
+    await this.commentRepository.softDelete({ id });
     return { deleted: true };
   }
 }
