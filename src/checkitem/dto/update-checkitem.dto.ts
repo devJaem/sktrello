97eba76c836871ca8f2c2f93fb22716c53f00795
jaemin <wjdwoals9292@gmail.com>
2024-07-16 20:17:41 +0900

    update: 오탈자 수정

--- src/checkitem/dto/update-checkitem.dto.ts
+++ src/checkitem/dto/update-checkitem.dto.ts
@@ -1,14 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { CHECK_MESSAGES } from 'src/constants/check-message.constant';
 
 export class UpdateCheckItemDto {
-  @IsString()
-  @IsNotEmpty({ message: '컨텐츠를 입력해주세요.' })
+  @IsString({ message: CHECK_MESSAGES.CHECKITEM.IS_STRING })
+  @IsNotEmpty({ message: CHECK_MESSAGES.CHECKITEM.CONTENT_NOT_EMPTY })
   @ApiProperty({ description: '체크 아이템 내용', example: 'checkItemContent' })
   content: string;
 
-  @IsBoolean()
-  @IsNotEmpty({ message: '상태를 입력해주세요' })
+  @IsBoolean({ message: CHECK_MESSAGES.CHECKITEM.IS_BOOLEAN })
+  @IsNotEmpty({ message: CHECK_MESSAGES.CHECKITEM.IS_DONE_NOT_EMPTY })
   @ApiProperty({ description: `작업 완료 여부`, example: true })
   isDone: boolean;
 }
--- src/constants/check-message.constant.ts
+++ src/constants/check-message.constant.ts
@@ -1,3 +1,5 @@
+import { IS_BOOLEAN, IS_NUMBER, IS_STRING } from "class-validator";
+
 // messages.ts
 export const CHECK_MESSAGES = {
   CHECKLIST: {
@@ -6,6 +8,8 @@ export const CHECK_MESSAGES = {
     DELETE: '체크리스트가 삭제되었습니다.',
     FOUND: '체크리스트가 조회 되었습니다.',
     NOT_FOUND: '체크리스트를 찾을 수 없습니다.',
+    CARD_ID_IS_NOT_EMPTY:'카드 아이디를 입력해주세요.',
+    TITLE_IS_NOT_EMPTY:'타이틀을 입력해주세요.',
     TARGET_NOT_FOUND: '목표 순서에 체크리스트가 존재하지 않습니다.',
     MOVE_WITHIN: '체크리스트가 이동되었습니다.',
     MOVE_TO_ANOTHER: '체크리스트가 다른 카드로 이동되었습니다.',
@@ -17,6 +21,11 @@ export const CHECK_MESSAGES = {
     DELETE: '체크리스트 아이템이 삭제되었습니다.',
     FOUND: '체크리스트 아이템이 조회 되었습니다.',
     NOT_FOUND: '체크리스트 아이템을 찾을 수 없습니다.',
+    CONTENT_NOT_EMPTY: '컨텐츠를 입력해 주세요.',
+    IS_DONE_NOT_EMPTY: '상태를 입력해 주세요.',
+    IS_STRING: '문자열을 입력해 주세요.',
+    IS_NUMBER: '숫자를 입력해 주세요.',
+    IS_BOOLEAN: 'Boolean을 입력해 주세요.',
     TARGET_NOT_FOUND: '목표 순서에 체크리스트 아이템이 존재하지 않습니다.',
     MOVE_WITHIN: '체크리스트 아이템이 이동되었습니다.',
     MOVE_TO_ANOTHER: '체크리스트 아이템이 다른 체크리스트로 이동되었습니다.',
