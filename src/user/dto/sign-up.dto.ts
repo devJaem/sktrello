0947c32e1421b8bdb5e5b97441e337bb50064446
9r3dflam3 <167046779+9r3dflam3@users.noreply.github.com>
2024-07-16 19:57:29 +0900

    feat : add api for swagger - signIn and signUp dto / user controller

--- src/user/dto/sign-up.dto.ts
+++ src/user/dto/sign-up.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsStrongPassword } from 'class-validator';
 import { USER_MESSAGES } from 'src/constants/user-message.constant';
 
 export class SignUpDto {
   @IsString()
   @IsNotEmpty({ message: USER_MESSAGES.USER.SIGNUP.EMAIL.EMPTY })
+  @ApiProperty({ example: 'example@sample.com' })
   email: string;
 
   @IsString()
@@ -12,13 +14,16 @@ export class SignUpDto {
     { minLength: 8, minSymbols: 1 },
     { message: USER_MESSAGES.USER.SIGNUP.PASSWORD.WEAKPASSWORD }
   )
+  @ApiProperty({ example: 'example!@' })
   password: string;
 
   @IsString()
   @IsNotEmpty({ message: USER_MESSAGES.USER.SIGNUP.PASSWORD.EMPTY })
+  @ApiProperty({ example: 'example!@' })
   passwordConfirm: string;
 
   @IsString()
   @IsNotEmpty({ message: USER_MESSAGES.USER.SIGNUP.NICKNAME.EMPTY })
+  @ApiProperty({ example: 'John Doe' })
   nickname: string;
 }
