3046f417d3365b00856a01b9807fd28de8ebcf34
Eomhyein <mickey_8688@naver.com>
2024-07-17 16:40:31 +0900

    🩹 fix : comment userguards 수정

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,9 +3,11 @@ import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { Comment } from './entities/comment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardModule } from 'src/board/board.module';
+import { BoardUser } from 'src/board/entities/board-user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment, BoardUser]), BoardModule],
   controllers: [CommentController],
   providers: [CommentService],
   exports: [CommentService],
