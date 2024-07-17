709ef000c241680602f7089ae23aa4c839c26930
정재민 <wjdwoals9292@gmail.com>
2024-07-17 22:57:53 +0900

    Revert "Delete src/checkItem directory"

---
+++ src/checkItem/entities/checkItem.entity.ts
@@ -0,0 +1,43 @@
+import { CheckList } from 'src/checkList/entities/checkList.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('check_items')
+export class CheckItem {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column({ unsigned: true })
+  checkListId: number;
+
+  @Column('text')
+  content: string;
+
+  @Column()
+  isDone: boolean;
+
+  @Column()
+  checkItemOrder: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt?: Date | null;
+
+  @ManyToOne(() => CheckList, (checkList) => checkList.checkItems, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  checkList: CheckList;
+}
